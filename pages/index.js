fd09b4cf2d77bde746438f417e12e224a958754d
nasalone <coffee4urheadandmyhead@gmail.com>
2022-07-20 15:17:05 +0100

    initial commit

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
-
+import Image from 'next/image'
 export default function Home() {
   return (
     <div className={styles.container}>
@@ -59,7 +59,7 @@ export default function Home() {
         >
           Powered by{' '}
           <span className={styles.logo}>
-            {/*<Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />*/}
+            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
           </span>
         </a>
       </footer>
